b776c80f533163306e4551c5c213fd85d3fd9896
justniar <salsaniarno@gmail.com>
2024-07-12 22:33:47 +0700

    fix: TextEncodingPolyfill

--- fe/src/app/recognize/page.tsx
+++ fe/src/app/recognize/page.tsx
@@ -1,10 +1,15 @@
 'use client';
-import { TextEncoder, TextDecoder } from 'text-encoding';
+
 import React, { useEffect, useRef, useState } from 'react';
 import * as faceapi from '@vladmandic/face-api';
 import { Grid, Box, Button } from '@mui/material';
 import PageContainer from '@/components/container/PageContainer';
 import StudentEnggagement from '@/components/monitoring/StudentEnggagement';
+
+const TextEncodingPolyfill = require('text-encoding');
+
+global.TextEncoder = TextEncodingPolyfill.TextEncoder;
+global.TextDecoder = TextEncodingPolyfill.TextDecoder;
 
 const modelPath = '/models/';
 const minScore = 0.2;
@@ -34,7 +39,7 @@ interface FaceApiResult {
     pitch: number;
     yaw: number;
   };
-  userId: string;  // Added userId here
+  userId: string;
 }
 
 let optionsSSDMobileNet: faceapi.SsdMobilenetv1Options;
@@ -43,7 +48,7 @@ let faceMatcher: faceapi.FaceMatcher;
 const Recognize: React.FC = () => {
   const [isWebcamActive, setIsWebcamActive] = useState(true);
   const [stream, setStream] = useState<MediaStream | null>(null);
-  const [labeledDescriptors, setLabeledDescriptors] = useState<faceapi.LabeledFaceDescriptors[]>([]);  // State to store labeled descriptors
+  const [labeledDescriptors, setLabeledDescriptors] = useState<faceapi.LabeledFaceDescriptors[]>([]);
 
   const videoRef = useRef<HTMLVideoElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -53,22 +58,26 @@ const Recognize: React.FC = () => {
   }, []);
 
   const loadModels = async () => {
-    await Promise.all([
-      faceapi.nets.ssdMobilenetv1.loadFromUri(modelPath),
-      faceapi.nets.ageGenderNet.loadFromUri(modelPath),
-      faceapi.nets.faceLandmark68Net.loadFromUri(modelPath),
-      faceapi.nets.faceRecognitionNet.loadFromUri(modelPath),
-      faceapi.nets.faceExpressionNet.loadFromUri(modelPath),
-    ]);
-    console.log('Face API models loaded');
-    optionsSSDMobileNet = new faceapi.SsdMobilenetv1Options({ minConfidence: minScore, maxResults });
-    const descriptors = await loadLabeledDescriptors();
-    setLabeledDescriptors(descriptors);
-    setupCamera();
+    try {
+      await faceapi.nets.ssdMobilenetv1.loadFromUri(modelPath);
+      await faceapi.nets.ageGenderNet.loadFromUri(modelPath);
+      await faceapi.nets.faceLandmark68Net.loadFromUri(modelPath);
+      await faceapi.nets.faceRecognitionNet.loadFromUri(modelPath);
+      await faceapi.nets.faceExpressionNet.loadFromUri(modelPath);
+
+      console.log('Face API models loaded');
+      optionsSSDMobileNet = new faceapi.SsdMobilenetv1Options({ minConfidence: minScore, maxResults });
+
+      const descriptors = await loadLabeledDescriptors();
+      setLabeledDescriptors(descriptors);
+      setupCamera();
+    } catch (error) {
+      console.error('Model loading error:', error);
+    }
   };
 
   const loadLabeledDescriptors = async () => {
-    const labels = ['Black Widow', 'Captain America', 'Hawkeye' , 'Jim Rhodes', 'Tony Stark', 'Thor', 'Captain Marvel']; // Replace with your user IDs
+    const labels = ['Black Widow', 'Captain America', 'Hawkeye', 'Jim Rhodes', 'Tony Stark', 'Thor', 'Captain Marvel']; // Replace with your user IDs
     const labeledDescriptors = await Promise.all(
       labels.map(async (label) => {
         const descriptions: Float32Array[] = [];
@@ -96,6 +105,7 @@ const Recognize: React.FC = () => {
       return null;
     }
 
+    let stream;
     const constraints: MediaStreamConstraints = {
       audio: false,
       video: {
@@ -106,8 +116,15 @@ const Recognize: React.FC = () => {
     };
 
     navigator.mediaDevices.getUserMedia(constraints).then((mediaStream) => {
-      setStream(mediaStream);
-      video.srcObject = mediaStream;
+      stream = mediaStream;
+      if (stream) {
+        setStream(stream);
+        video.srcObject = stream;
+      } else {
+        console.error('Camera Error: stream empty');
+        return null;
+      }
+
       video.onloadeddata = () => {
         canvas.width = video.videoWidth;
         canvas.height = video.videoHeight;
@@ -116,12 +133,13 @@ const Recognize: React.FC = () => {
       };
     }).catch((err) => {
       console.error(`Camera Error: ${(err as Error).message || err}`);
+      return null;
     });
   };
 
   const detectVideo = async (video: HTMLVideoElement, canvas: HTMLCanvasElement) => {
     if (!video || video.paused) return;
-  
+
     const t0 = performance.now();
     try {
       const result = await faceapi
@@ -130,7 +148,7 @@ const Recognize: React.FC = () => {
         .withFaceDescriptors()
         .withFaceExpressions()
         .withAgeAndGender();
-  
+
       const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors, 0.6);
       const faceApiResults = result.map((res) => {
         const bestMatch = faceMatcher.findBestMatch(res.descriptor);
@@ -148,7 +166,7 @@ const Recognize: React.FC = () => {
           userId: bestMatch.toString()
         };
       });
-  
+
       const fps = 1000 / (performance.now() - t0);
       drawFaces(canvas, faceApiResults, fps.toLocaleString());
       requestAnimationFrame(() => detectVideo(video, canvas));
@@ -156,7 +174,7 @@ const Recognize: React.FC = () => {
       console.error(`Detect Error: ${JSON.stringify(err)}`);
     }
   };
-  
+
   const drawFaces = (canvas: HTMLCanvasElement, data: FaceApiResult[], fps: string) => {
     const ctx = canvas.getContext('2d', { willReadFrequently: true });
     if (!ctx) return;
@@ -164,7 +182,7 @@ const Recognize: React.FC = () => {
     ctx.font = 'small-caps 20px "Segoe UI"';
     ctx.fillStyle = 'white';
     ctx.fillText(`FPS: ${fps}`, 10, 25);
-  
+
     for (const person of data) {
       ctx.lineWidth = 1;
       ctx.strokeStyle = 'deepskyblue';
@@ -174,17 +192,66 @@ const Recognize: React.FC = () => {
       ctx.rect(person.detection.box.x, person.detection.box.y, person.detection.box.width, person.detection.box.height);
       ctx.stroke();
       ctx.globalAlpha = 1;
-  
+
       const expression = Object.entries(person.expressions).sort((a, b) => b[1] - a[1]);
       ctx.fillStyle = 'lightblue';
       ctx.fillText(`gender: ${Math.round(100 * person.genderProbability)}% ${person.gender}`, person.detection.box.x, person.detection.box.y - 40);
       ctx.fillText(`ekspresi: ${Math.round(100 * expression[0][1])}% ${expression[0][0]}`, person.detection.box.x, person.detection.box.y - 20);
       ctx.fillText(`User ID: ${person.userId}`, person.detection.box.x, person.detection.box.y + person.detection.box.height + 20);
+
+      // Call Predict API
+      const predictResult = predict(person.detection.box, canvas);
+      ctx.fillText(`Fokus: ${predictResult.expression}`, person.detection.box.x, person.detection.box.y);
+      console.log(predictResult);
+      console.log(predictResult.expression);
     }
   };
 
-  const handleToggleWebcam = () => {
-    setIsWebcamActive((prevIsActive) => !prevIsActive);
+  const cropCanvas = (canvas: HTMLCanvasElement, box: faceapi.Box) => {
+    const croppedCanvas = document.createElement('canvas');
+    const ctx = croppedCanvas.getContext('2d');
+    croppedCanvas.width = box.width;
+    croppedCanvas.height = box.height;
+    ctx?.drawImage(canvas, box.x, box.y, box.width, box.height, 0, 0, box.width, box.height);
+    return croppedCanvas;
+  };
+
+  const predict = (box: faceapi.Box, canvas: HTMLCanvasElement) => {
+    try {
+      const formData = new FormData();
+      const croppedCanvas = cropCanvas(canvas, box);
+      const blob = dataURLtoBlob(croppedCanvas.toDataURL());
+      formData.append('frame', blob, 'snapshot.png');
+
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', 'http://localhost:5000/predict', false); // false for synchronous request
+      xhr.send(formData);
+
+      if (xhr.status === 200) {
+        return JSON.parse(xhr.responseText) as Prediction;
+      } else {
+        throw new Error(`Request failed with status ${xhr.status}`);
+      }
+    } catch (error) {
+      console.error('Prediction error:', error);
+      return { user: 'unknown', expression: 'unknown', time: '' };
+    }
+  };
+
+  const dataURLtoBlob = (dataURL: string): Blob => {
+    const parts = dataURL.split(',');
+    const byteString = atob(parts[1]);
+    const mimeString = parts[0].split(':')[1].split(';')[0];
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+    return new Blob([ab], { type: mimeString });
+  };
+
+  const handleWebcamToggle = () => {
+    setIsWebcamActive(!isWebcamActive);
     if (stream) {
       stream.getTracks().forEach((track) => track.stop());
       setStream(null);
@@ -194,31 +261,29 @@ const Recognize: React.FC = () => {
   };
 
   return (
-    <PageContainer title="Detection" description="this is Detection page">
-      <Box>
-        <Grid container spacing={3}>
-          <Grid item xs={12} lg={12}>
-            <Button variant="contained" color="primary" onClick={handleToggleWebcam}>
-              {isWebcamActive ? 'Turn Off Webcam' : 'Turn On Webcam'}
-            </Button>
-            <Box
-              sx={{
-                position: 'relative',
-                overflow: 'hidden',
-                marginTop: '20px',
-                borderRadius: '10px',
-                boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
-              }}
-            >
-              <video ref={videoRef} style={{ width: '100%', height: 'auto' }} />
-              <canvas ref={canvasRef} style={{ position: 'absolute', top: 0, left: 0 }} />
-            </Box>
-          </Grid>
-          <Grid item xs={12} lg={12}>
-            <StudentEnggagement />
-          </Grid>
+    <PageContainer title="Monitoring Engagement">
+      <Grid container spacing={0} justifyContent="center" sx={{ height: '100vh' }}>
+        <Grid item xs={12} sm={6} lg={8}>
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            sx={{ height: '100%', width: '100%' }}
+          >
+            {isWebcamActive ? (
+              <video ref={videoRef} width="100%" height="100%" autoPlay muted />
+            ) : (
+              <canvas ref={canvasRef} width="100%" height="100%" />
+            )}
+          </Box>
+          <Button variant="contained" color="primary" onClick={handleWebcamToggle}>
+            {isWebcamActive ? 'Stop Webcam' : 'Start Webcam'}
+          </Button>
         </Grid>
-      </Box>
+        <Grid item xs={12} sm={6} lg={4}>
+          <StudentEnggagement />
+        </Grid>
+      </Grid>
     </PageContainer>
   );
 };
